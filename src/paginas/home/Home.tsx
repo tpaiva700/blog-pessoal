6e2d4c9bb03ac665e3ec6ced232cb6d8618c44d0
tpaiva700 <thaisferreirapaiva19@gmail.com>
2024-04-25 18:54:11 -0300

    Iniciando o projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.pinimg.com/564x/f1/97/0a/f1970a8b5bdf920a2e1977a28e2e8c77.jpg" alt="ninja" className="img"/>
+        </>
+    );
+}
+
+export default Home;
